6a1a28ef69716787932deb9c087bab4cad53b38b
Yermochenko <yermochenko@gmail.com>
2016-10-27 20:08:26 +0200

    Логика Добавление составного фильтра с операцией И (через шаблон проектирования "Компоновщик")

---
+++ src/logic/AndFilter.java
@@ -0,0 +1,18 @@
+package logic;
+
+import domain.Vaucher;
+
+public class AndFilter implements Filter {
+	private Filter a;
+	private Filter b;
+
+	public AndFilter(Filter a, Filter b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public boolean isSatisfy(Vaucher vaucher) {
+		return a.isSatisfy(vaucher) && b.isSatisfy(vaucher);
+	}
+}
